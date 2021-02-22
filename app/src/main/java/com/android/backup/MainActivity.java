d48908b860b05181e077c5499eee1dd83c4f9286
tiennvh <tiennvh@bkav.com>
2021-02-22 18:03:58 +0700

    begin

---
+++ app/src/main/java/com/android/backup/MainActivity.java
@@ -0,0 +1,32 @@
+package com.android.backup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText  username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.password);
+        findViewById(R.id.bt_reset).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
